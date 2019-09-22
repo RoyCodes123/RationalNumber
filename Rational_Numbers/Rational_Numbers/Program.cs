e4dd99fe6fd2f2992411929f6fdea569662fa7db
RoyCodes123 <50462856+RoyCodes123@users.noreply.github.com>
2019-09-22 23:47:58 +0300

    Add files via upload

---
+++ Rational_Numbers/Rational_Numbers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rational_Numbers
+{
+    class Program
+    {
+        ///<summary> Function for addition multiplies numerator of num1 by denominator 
+        /// of num2 and vice versa then adds to get new numerator of new number, multiplies the two denominators to get new denominator of new number</summary>
+        static Rational Addition(Rational num1, Rational num2) { 
+            int num, den;
+            num = num1.GetNumerator() * num2.GetDenom() + num2.GetNumerator() * num1.GetDenom();
+            den = num1.GetDenom() * num2.GetDenom();
+            Rational p1 = new Rational(num, den);
+            return p1;
+        }
+
+        ///<summary> Function for subtraction multiplies numerator of num1 by denominator 
+        /// of num2 and vice versa then subtracts to get new numerator of new number, multiplies the two denominators to get new denominator of new number</summary>
+        static Rational Subtraction(Rational num1, Rational num2)
+        {
+            int num, den;
+            num = num1.GetNumerator() * num2.GetDenom() - num2.GetNumerator() * num1.GetDenom();
+            den = num1.GetDenom() * num2.GetDenom();
+            Rational p1 = new Rational(num, den);
+            return p1;
+        }
+
+        static void Main(string[] args)
+        {
+            Rational num = new Rational(1, 4);
+            Rational num2 = new Rational(2, 8);
+
+            Console.WriteLine(num.IsEqual(num2));
+            Console.WriteLine(num.GetDenom());
+            Console.WriteLine(num.GetNumerator());
+            Console.WriteLine(num.Divide(num2));
+            Console.WriteLine(num.Multiply(num2));
+            Console.WriteLine(Addition(num,num2));
+            Console.WriteLine(Subtraction(num, num2));
+        }
+    }
+}
---
+++ Rational_Numbers/Rational_Numbers/Rational.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rational_Numbers
+{
+    class Rational
+    {
+        private int x;
+        private int y;
+
+        public Rational(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int GetNumerator() { return this.x; }
+        public int GetDenom() { return this.y; }
+
+        public bool IsEqual(Rational num) {
+
+            return num.x * this.y == num.y * this.x;
+        }
+
+        public Rational Multiply(Rational num) {
+            Rational p1 = new Rational(this.x * num.x, this.y * num.y);
+            return p1;
+        }
+
+        public Rational Divide(Rational num) {
+            if (num.x == 0) {
+                return null;
+            }
+            Rational p1 = new Rational(this.x * num.y, this.y * num.x);
+            return p1;
+        }
+
+        public override string ToString()
+        {
+            return "<" + this.x + ">/<" + this.y + ">";
+        }
+    }
+}
